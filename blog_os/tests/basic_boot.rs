e23b0eba371020d8dd54fd8bab326c444e8fd02e
Tyler Moquin <124649324+plusplusminusminus@users.noreply.github.com>
2026-06-26 00:19:53 -0700

    Add testing

--- blog_os/tests/basic_boot.rs
+++ blog_os/tests/basic_boot.rs
@@ -10,23 +10,17 @@ use blog_os::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
