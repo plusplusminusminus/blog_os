0b4ce495dbafc2bf0991b23f7b9457f82e3fe7bf
Tyler Moquin <124649324+plusplusminusminus@users.noreply.github.com>
2026-06-26 23:09:43 -0700

    Implement CPU Exceptions

--- blog_os/src/lib.rs
+++ blog_os/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
 #![cfg_attr(test, no_main)]
+#![feature(abi_x86_interrupt)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
@@ -9,9 +10,14 @@ use core::panic::PanicInfo;
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
 pub trait Testable {
     fn run(&self) -> ();
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
 
 impl<T> Testable for T
@@ -40,14 +46,6 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     loop {}
 }
 
-/// Entry point for `cargo test`
-#[cfg(test)]
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    test_main();
-    loop {}
-}
-
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -68,4 +66,13 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
+}
+
+/// Entry point for `cargo test`
+#[cfg(test)]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    init();      // new
+    test_main();
+    loop {}
 }
